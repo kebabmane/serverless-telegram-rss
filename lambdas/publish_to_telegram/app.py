a9e389677871ef07bea9301683ac380cc063998d
Rhys Evans <kebabman@icloud.com>
2020-03-03 11:33:00 +1100

    resolved the issue with json and dynamo records

--- lambdas/publish_to_telegram/app.py
+++ lambdas/publish_to_telegram/app.py
@@ -24,8 +24,31 @@ def configure_telegram():
 
     return telegram.Bot(TELEGRAM_TOKEN)
 
+def extract_values(obj, key):
+    """Pull all values of specified key from nested JSON."""
+    arr = []
+
+    def extract(obj, arr, key):
+        """Recursively search for values of key in JSON tree."""
+        if isinstance(obj, dict):
+            for k, v in obj.items():
+                if isinstance(v, (dict, list)):
+                    extract(v, arr, key)
+                elif k == key:
+                    arr.append(v)
+        elif isinstance(obj, list):
+            for item in obj:
+                extract(item, arr, key)
+        return arr
+
+    results = extract(obj, arr, key)
+    return results
+
 def lambda_handler(event, context):
+    TELEGRAM_CHAT_ID = os.environ.get('TELEGRAM_CHAT_ID')
     bot = configure_telegram()
     for record in event['Records']:
-        bot.send_message(record)
+        d = json.loads(record['body'])
+        print(d['dynamodb']['Keys']['id']['S'])
+       # bot.send_message(text=record['body']['NewImage']['id']['S'], chat_id=TELEGRAM_CHAT_ID)
     print('Successfully processed %s records.' % str(len(event['Records'])))
