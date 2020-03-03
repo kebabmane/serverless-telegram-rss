d63aef98bf31480f1557fc66aa85fd1481ee8f92
Rhys Evans <kebabman@icloud.com>
2020-03-03 14:06:18 +1100

    need to add uniqueness and fix some other small bugs but it's working

---
+++ lambdas/parse_feeds_to_dynamodb/app.py
@@ -0,0 +1,34 @@
+from __future__ import print_function
+import json
+import os
+import logging
+import boto3
+import feedparser
+from boto3.dynamodb.conditions import Key, Attr
+
+# Logging is cool!
+logger = logging.getLogger()
+if logger.handlers:
+    for handler in logger.handlers:
+        logger.removeHandler(handler)
+logging.basicConfig(level=logging.INFO)
+
+# Bring in the OS Vars from Lambda Runtime
+# queue_url = os.environ['LAMBDA_QUEUE']
+dynamodb_feeds_table_name = os.environ['FEEDS_DYNAMODB_TABLE']
+dynamodb_entries_table_name = os.environ['DYNAMODB_TABLE']
+
+def lambda_handler(event, context):
+    dynamodb = boto3.resource('dynamodb')
+    feeds_table = dynamodb.Table(dynamodb_feeds_table_name)
+    entries_table = dynamodb.Table(dynamodb_entries_table_name)
+
+    response = feeds_table.scan()
+    for item in response['Items']:
+        rss_link = item['id']
+        d = feedparser.parse(rss_link)
+        for entry in d.entries:
+            print(entry.link)
+            entries_table.put_item(
+                Item={'id': entry.link}
+            )
